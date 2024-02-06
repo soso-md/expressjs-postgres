10fe5b40cee7292357d6b87c8917bc37e7789ebf
Sophie Raps <s.raps@onveda.de>
2024-02-06 23:38:16 +0100

    Added: CRUD

--- src/students/controller.js
+++ src/students/controller.js
@@ -35,12 +35,47 @@ const createStudent = (req, res) => {
     // add student to db
     pool.query(query.addStudent, [name, email, age, dob], (error, results) => {
         if (error) throw error;
-        res.status(201).send(`Student added with ID: ${results}`);
+        res.status(201).send(`Student added successfully`);
     })
+}
+
+const removeStudent = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    pool.query(query.getStudentById, [id], (error, results) => {
+        const noStudentFound = !results.rows.length;
+        if (noStudentFound){
+            res.send("Student not found");
+        }
+
+        pool.query(query.removeStudent, [id], (error, results) => {
+            if (error) throw error;
+            res.status(200).send(`Student deleted`);
+        });
+    });
+}
+
+const updateStudent = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { name } = req.body
+
+    pool.query(query.getStudentById, [id], (error, results) => {
+        const noStudentFound = !results.rows.length;
+        if (noStudentFound){
+            res.send("Student not found");
+        }
+
+        pool.query(query.updateStudent, [name, id], (error, results) => {
+            if (error) throw error;
+            res.status(200).send(`Student ${name} updated successfully`);
+        });
+    });
 }
 
 module.exports = {
     getStudents,
     getStudentById,
     createStudent,
+    removeStudent,
+    updateStudent,
 }
--- src/students/routes.js
+++ src/students/routes.js
@@ -6,5 +6,7 @@ const controller = require('../../src/students/controller')
 router.get('/', controller.getStudents);
 router.get('/:id', controller.getStudentById);
 router.post('/', controller.createStudent);
+router.delete('/:id', controller.removeStudent);
+router.put('/:id', controller.updateStudent);
 
 module.exports = router;
